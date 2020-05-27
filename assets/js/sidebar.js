f611c8b6ffd79bafce06c4067b47c3955cd1a866
hailey.lum <haileyy.lum@gmail.com>
2020-05-27 15:14:46 +0800

    Added >> submenu Changed >> Title >> from `My Dashboard` to ` Retailer Jaya Dashboard`

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -1,22 +1,23 @@
 // SideBar
 $(document).ready(function () {
-    "use strict";
+  "use strict";
 
-    var fullHeight = function () {
-        $(".js-fullheight").css("height", $(window).height());
-        $(window).resize(function () {
-            $(".js-fullheight").css("height", $(window).height());
-        });
-    };
-    fullHeight();
-
-    $("#collapse-sidebar").on("click", function () {
-        $("#sidebar").toggleClass("active");
-        $("#content").toggleClass("collapse-content");
-        $("#Header").toggleClass("collapse-header");
+  var fullHeight = function () {
+    $(".js-fullheight").css("height", $(window).height());
+    $(window).resize(function () {
+      $(".js-fullheight").css("height", $(window).height());
     });
+  };
+  fullHeight();
 
-})(jQuery)
+  $("#collapse-sidebar").on("click", function () {
+    $("#sidebar").toggleClass("active");
+    $("#content").toggleClass("collapse-content");
+    $("#Header").toggleClass("collapse-header");
+  });
 
-
-
+  //   $("#sidebar ul li.active").on("click", function () {
+  //     // $("#hideicon").toggleClass("active");
+  //     $(".submenu").toggleClass("active");
+  //   });
+})(jQuery);
